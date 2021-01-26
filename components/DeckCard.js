66ddc3ac29d0a6185aa3cdbdc262ec729267f78e
rahafo <rahaf.o.omar@gmail.com>
2021-01-26 03:20:33 +0300

    fix card flip

--- components/DeckCard.js
+++ components/DeckCard.js
@@ -1,90 +1,57 @@
 import {Animated, StyleSheet, Text, TouchableOpacity, View} from 'react-native'
 import React, {Component} from 'react';
 import {gray, lightGray, lightPurple} from "../utils/colors";
+import CardFlip from "react-native-card-flip";
 
 export default class DeckCard extends Component {
 
-    flipCard() {
-        if (this.value >= 90) {
-            Animated.spring(this.animatedValue,{
-                toValue: 0,
-                friction: 8,
-                tension: 10
-            }).start();
-        } else {
-            Animated.spring(this.animatedValue,{
-                toValue: 180,
-                friction: 8,
-                tension: 10
-            }).start();
+    state = {
+        cardView: "front",
+    };
+
+    flipCard(view) {
+        this.card.flip();
+        this.setState({cardView: view}, () => console.log(this.state.cardView))
+    }
+
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.card.question !== this.props.card.question && this.state.cardView !== "front") {
+            this.flipCard("front")
         }
-
     }
 
-    componentWillMount() {
-
-        // Flipcard animation
-        this.animatedValue = new Animated.Value(0);
-        this.value = 0;
-        this.animatedValue.addListener(({value}) => {
-            this.value = value;
-        });
-        this.frontInterpolate = this.animatedValue.interpolate({
-            inputRange: [0, 180],
-            outputRange: ['0deg', '180deg'],
-        });
-        this.backInterpolate = this.animatedValue.interpolate({
-            inputRange: [0, 180],
-            outputRange: ['180deg', '360deg']
-        });
-        this.frontOpacity = this.animatedValue.interpolate({
-            inputRange: [89, 90],
-            outputRange: [1, 0]
-        });
-
-        this.backOpacity = this.animatedValue.interpolate({
-            inputRange: [89, 90],
-            outputRange: [0, 1]
-        });
-    }
 
     render() {
         const {card} = this.props;
-        const frontAnimatedStyle = {
-            transform: [
-                {rotateY: this.frontInterpolate}
-            ]
-        };
-        const backAnimatedStyle = {
-            transform: [
-                {rotateY: this.backInterpolate}
-            ]
-        };
+
         return (
             <View style={styles.container}>
-                <TouchableOpacity onPress={() => this.flipCard()}>
-                    <Animated.View
-                        style={[styles.flipCard1, styles.card, frontAnimatedStyle, {opacity: this.frontOpacity}]}>
-                        <View>
-                            <Text style={{fontSize: 25, fontWeight: "bold", color: lightGray}}>
-                                {card.question}
-                            </Text>
-                            <Text style={{fontSize: 15, marginTop: 15, color: lightGray}}>
-                                Click to view answer
-                            </Text>
-                        </View>
-                    </Animated.View>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => this.flipCard()}>
-                    <Animated.View
-                        style={[styles.flipCard1, styles.flipCardBack1, styles.card, backAnimatedStyle, {opacity: this.backOpacity}]}>
-                        <View>
-                            <Text style={{fontSize: 25, fontWeight: "bold", color: lightGray}}>
-                                {card.answer}
-                            </Text>
-                        </View>
-                    </Animated.View>
-                </TouchableOpacity>
+                <CardFlip style={styles.cardFlipContainer} ref={card => (this.card = card)}>
+                    <TouchableOpacity onPress={() => this.flipCard("back")}>
+                        <Animated.View
+                            style={styles.card}>
+                            <View>
+                                <Text style={{fontSize: 25, fontWeight: "bold", color: lightGray}}>
+                                    {card.question}
+                                </Text>
+                                <Text style={{fontSize: 15, marginTop: 15, color: lightGray}}>
+                                    Click to view answer
+                                </Text>
+                            </View>
+                        </Animated.View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => this.flipCard("front")}>
+                        <Animated.View
+                            style={styles.card}>
+                            <View>
+                                <Text style={{fontSize: 25, fontWeight: "bold", color: lightGray}}>
+                                    {card.answer}
+                                </Text>
+                            </View>
+                        </Animated.View>
+                    </TouchableOpacity>
+                </CardFlip>
             </View>
         )
     }
@@ -95,13 +62,13 @@ const styles = StyleSheet.create({
     container: {
         justifyContent: 'center',
         alignItems: 'center',
+        height: "30%"
     },
     card: {
         backgroundColor: lightPurple,
         flexDirection: "row",
         borderRadius: 10,
         width: 380,
-        // height: containerWidth,
         padding: 40,
         marginTop: 10,
         justifyContent: 'center',
@@ -114,12 +81,11 @@ const styles = StyleSheet.create({
             height: 3
         },
     },
-    flipCard1: {
-        backfaceVisibility: 'hidden',
+    cardFlipContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 10,
+        marginRight: "90%"
     },
-    flipCardBack1 : {
-        position: "absolute",
-        bottom: 0,
-        left:-190
-    }
 });
